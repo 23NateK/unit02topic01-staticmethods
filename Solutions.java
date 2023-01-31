86751ff76b5292b39c3ef4dd5f5a9da8bd4f6ad0
23NateK <n.kimber22@gmail.com>
2023-01-31 12:09:38 -0600

    magically finished max3 + overload and odd

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,57 @@
 public class Solutions {
-    public static void main(String[] args) {
-        
+
+    //max3
+    public static double max3(double a, double b, double c){
+        if (a>b&&a>c){
+            return a;
+        }
+        if (b>a&&b>c){
+            return b;
+        }
+        return c;
     }
-}
+    //overload max3
+    public static int max3(int a, int b, int c){
+        if (a>b&&a>c){
+            return a;
+        }
+        if (b>a&&b>c){
+            return b;
+        }
+        return c;
+    }
+
+    //odd
+    public static boolean odd(boolean a,boolean b, boolean c){
+        int truenum = 0;
+        if (a){
+            truenum+=1;
+        }
+        if (b){
+            truenum+=1;
+        }
+        if (c){
+            truenum+=1;
+        }
+        if (truenum==1||truenum==3){
+            return true;
+        }
+        return false;
+    }
+
+    //majority
+    public static boolean majority(boolean a,boolean b, boolean c){
+
+    }
+
+
+    
+    public static void main(String[] args) {
+        System.out.println(max3(2.5,4,2.5));
+        System.out.println(odd(true,true,true));
+
+
+
+
+    }
+} // javac Solutions.java;java Solutions
